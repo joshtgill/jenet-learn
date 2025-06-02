7cd38b9bb148185209d09cb872ff8dc795336854
Josh Gillette <joshtgillette@gmail.com>
2025-06-01 20:14:08 -0500

    Squashed commit of the following:
    commit fac5bd7f702cf2378263487e39917883a1402609
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Sun Jun 1 18:58:28 2025 -0500
    
        Train and query with Bert sequence classification
    
    commit c9c676fa34c605404bba62758e72290dc3e6f95a
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Sun Jun 1 18:47:25 2025 -0500
    
        Use text label in Pandas df instead of number
    
    commit b6ac469cac73620372a5056617e60a2889efb829
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Sun Jun 1 18:46:31 2025 -0500
    
        Improve number adapter to include decimals
    
    commit 4d4b35fd04994ddf0992ad1c570f05d580c34559
    Author: Josh Gillette <joshtgillette@gmail.com>
    Date:   Sun Jun 1 18:45:26 2025 -0500
    
        Remove model sub-repository

--- main.py
+++ main.py
@@ -9,43 +9,36 @@ from data.adapters.address_adapter import AddressAdapter
 from data.adapters.text_adapter import TextAdapter
 from dataset import Dataset as Dataset
 import learner as learner
-from model.line_vectorizer import LineVectorizer
-from model.model import Model
 
 
 DATA_RES_PATH = 'data/res/'
 MODEL_PATH = 'model/'
 DATA_SOURCES = {
-    'number': NumberAdapter(),
-    'price': PriceAdapter(),
-    'date': DateAdapter(),
-    'time': TimeAdapter(),
-    'name': NameAdapter(DATA_RES_PATH + 'first_names.txt',
+    'NUMBER': NumberAdapter(),
+    'PRICE': PriceAdapter(),
+    'DATE': DateAdapter(),
+    'TIME': TimeAdapter(),
+    'NAME': NameAdapter(DATA_RES_PATH + 'first_names.txt',
                         DATA_RES_PATH + 'last_names.txt'),
-    'city': CityAdapter(DATA_RES_PATH + 'cities.txt'),
-    'address': AddressAdapter(DATA_RES_PATH + 'addresses.txt'),
-    'order number': NumberAdapter(min_length=5,
+    'CITY': CityAdapter(DATA_RES_PATH + 'cities.txt'),
+    'ADDRESS': AddressAdapter(DATA_RES_PATH + 'addresses.txt'),
+    'ORDER_NUMBER': NumberAdapter(min_length=5,
                                   max_length=20,
                                   contain_digits=True,
                                   contain_letters=True,
+                                  contains_decimal=False,
                                   prefix=''),
-    'tracking number': NumberAdapter(min_length=10,
+    'TRACKING_NUMBER': NumberAdapter(min_length=10,
                                      max_length=20,
                                      contain_digits=True,
                                      contain_letters=True,
+                                     contains_decimal=False,
                                      prefix='T-'),
-    'airport': TextAdapter(DATA_RES_PATH + 'airport_codes.txt',
+    'AIRPORT': TextAdapter(DATA_RES_PATH + 'airport_codes.txt',
                            DATA_RES_PATH + 'airport_names.txt'),
-    'language': TextAdapter(DATA_RES_PATH + 'language.txt')
+    'LANGUAGE': TextAdapter(DATA_RES_PATH + 'language.txt')
 }
 dataset = Dataset(DATA_RES_PATH)
-
-
-def query(line):
-    pred = Model.load(MODEL_PATH).query(line)
-    label = next(label for type, (label, _) in enumerate(DATA_SOURCES.items())
-                 if type == pred)
-    print(f'{line} is of type {label}')
 
 
 if __name__ == '__main__':
@@ -53,9 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--make', type=int, metavar='<n>', help='make a dataset with n total samples')
     parser.add_argument('-t', '--train', action='store_true', help='train the model with the stored dataset')
     parser.add_argument('-q', '--query', type=str, metavar='<line>', help='query the stored model on a given line')
-    parser.add_argument('-r', '--train-ratio', type=float, metavar='<train ratio>', help='the ratio of data to train on', default=0.80)
-    parser.add_argument('-b', '--batches', type=int, metavar='<batch size>', help='the batch size to train on', default=64)
-    parser.add_argument('-e', '--epochs', type=int, metavar='<number of epochs>', help='the number of epochs to train over', default=5)
+    parser.add_argument('-b', '--batch_size', type=int, metavar='<batch size>', help='the batch size to train on', default=learner.DEFAULT_BATCH_SIZE)
+    parser.add_argument('-e', '--num_epochs', type=int, metavar='<number of epochs>', help='the number of epochs to train over', default=learner.DEFAULT_BATCH_SIZE)
 
     print()
     args = parser.parse_args()
@@ -63,12 +55,10 @@ if __name__ == '__main__':
         dataset.make(DATA_SOURCES, args.make)
         print()
     if args.train:
-        dataset.load(LineVectorizer)
-        learner.learn(dataset, args.train_ratio, args.batches, args.epochs, MODEL_PATH)
+        learner.train("data/res/dataset.csv", args.batch_size, args.num_epochs)
         print()
     if args.query:
-        query(args.query)
-        print()
+        print(f'{args.query} is of type {learner.query(args.query)}')
 
     if not (args.make or args.train or args.query):
         parser.print_help()
